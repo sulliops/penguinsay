4e1debff07ab6b33533d248105ade30d65e11477
Owen Sullivan <sulliops@gmail.com>
2024-11-07 13:20:35 -0500

    Complete vrclient

--- vulnreport/client/vrclient.go
+++ vulnreport/client/vrclient.go
@@ -2,21 +2,36 @@ package main
 
 // Import necessary packages
 import (
-	"fmt"
 	"log"
+	"net"
 	"os/exec"
 )
 
 func main() {
-	// Run linux-exploit-suggester
+	// Connect to vulnreport listening server
+	conn, err := net.Dial("tcp", "babbage12.computing.clemson.edu:9090")
+	// Error check connection
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// Run linux-exploit-suggester and get output
 	lesDir := "/usr/share/linux-exploit-suggester"
 	out, err := exec.Command(lesDir+"/les.sh", "--checksec").Output()
-
 	// Check for runtime errors
 	if err != nil {
 		log.Fatal(err)
 	} else {
 		// Send output of linux-exploit-suggester to external listening server
-		fmt.Println(string(out))
+		_, err := conn.Write([]byte(string(out)))
+		// Error check connection
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		// Close the connection
+		conn.Close()
 	}
 }
