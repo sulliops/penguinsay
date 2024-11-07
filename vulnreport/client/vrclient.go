67cd99014d0499fe47d46a104bf98291e466058a
Owen Sullivan <sulliops@gmail.com>
2024-11-07 13:08:30 -0500

    Fix scripts to run LES properly

--- vulnreport/client/vrclient.go
+++ vulnreport/client/vrclient.go
@@ -2,6 +2,7 @@ package main
 
 // Import necessary packages
 import (
+	"fmt"
 	"log"
 	"os/exec"
 )
@@ -9,13 +10,13 @@ import (
 func main() {
 	// Run linux-exploit-suggester
 	lesDir := "/usr/share/linux-exploit-suggester"
-	out, err := exec.Command("sh", lesDir+"/les.sh", "--checksec").Output()
+	out, err := exec.Command(lesDir+"/les.sh", "--checksec").Output()
 
 	// Check for runtime errors
 	if err != nil {
 		log.Fatal(err)
 	} else {
 		// Send output of linux-exploit-suggester to external listening server
-
+		fmt.Println(string(out))
 	}
 }
